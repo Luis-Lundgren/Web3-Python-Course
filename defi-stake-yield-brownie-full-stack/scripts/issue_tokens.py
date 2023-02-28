5cdcde6836f60b499a4ac33fb24109f0e8fba29f
Luis Lundgren <126507747+Luis-Lundgren@users.noreply.github.com>
2023-02-28 11:16:05 -0800

    Add files via upload

---
+++ defi-stake-yield-brownie-full-stack/scripts/issue_tokens.py
@@ -0,0 +1,12 @@
+"""
+I wrote this code by taking the Free Code Camp:
+Solidity, Blockchain, and Smart Contract Course – Beginner to Expert Python Tutorial
+links:
+Youtube: https://youtu.be/M576WGiDBdQ
+GitHub: https://github.com/smartcontractkit/full-blockchain-solidity-course-py
+"""
+from scripts.helpful_scripts import issue_tokens
+
+
+def main():
+    issue_tokens()
---
+++ defi-stake-yield-brownie-full-stack/scripts/update_front_end.py
@@ -0,0 +1,12 @@
+"""
+I wrote this code by taking the Free Code Camp:
+Solidity, Blockchain, and Smart Contract Course – Beginner to Expert Python Tutorial
+links:
+Youtube: https://youtu.be/M576WGiDBdQ
+GitHub: https://github.com/smartcontractkit/full-blockchain-solidity-course-py
+"""
+from scripts.deploy import update_front_end
+
+
+def main():
+    update_front_end()
